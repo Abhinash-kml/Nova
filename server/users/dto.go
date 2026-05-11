7549fa0818cee8f7ec682805aff77dd91d2f02ec
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-05-12 00:42:07 +0530

    Implemented main models for users

--- server/users/dto.go
+++ server/users/dto.go
@@ -1,1 +1,30 @@
 package users
+
+import "github.com/google/uuid"
+
+type UserCreateDTO struct {
+	Username    string `json:"username"`
+	DisplayName string `json:"display_name"`
+	Email       string `json:"email"`
+	Country     string `json:"country"`
+	State       string `json:"state"`
+	LangTag     string `json:"lang_tag"`
+}
+
+func NewUserCreateDTO(user *User) UserCreateDTO {
+	return UserCreateDTO{
+		Username:    user.Username,
+		DisplayName: user.DisplayName,
+		Email:       user.Email,
+		Country:     user.Country,
+		State:       user.State,
+		LangTag:     user.LangTag,
+	}
+}
+
+type UserUpdateDTO struct {
+	Id       uuid.UUID `json:"id"`
+	Field    string    `json:"field"`
+	DataType string    `json:"datatype"`
+	Value    string    `json:"value"`
+}
--- server/users/model.go
+++ server/users/model.go
@@ -1,1 +1,37 @@
 package users
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id          uuid.UUID `json:"id" redis:"id"`
+	Username    string    `json:"username" redis:"username"`
+	DisplayName string    `json:"display_name" redis:"display_name"`
+	Email       string    `json:"email" redis:"email"`
+	Country     string    `json:"country" redis:"country"`
+	State       string    `json:"state" redis:"state"`
+	AvatarURL   string    `json:"avatar_url" redis:"avatar_url"`
+	LangTag     string    `json:"lang_tag" redis:"lang_tag"`
+	Timezone    string    `json:"timezone" redis:"timezone"`
+	CreatedAt   time.Time `json:"created_at" redis:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" redis:"updated_at"`
+	VerifiedAt  time.Time `json:"verified_at" redis:"verfied_at"`
+}
+
+func NewUser(id uuid.UUID, username, displayname, email, country, state, langtag, timezone string) *User {
+	return &User{
+		Id:          id,
+		Username:    username,
+		DisplayName: displayname,
+		Email:       email,
+		Country:     country,
+		State:       state,
+		LangTag:     langtag,
+		Timezone:    timezone,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
