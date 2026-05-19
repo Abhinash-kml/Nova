4966fefc2b10e45616e4a44fc3442dc1c1e60ff0
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-05-19 16:12:23 +0530

    Partial untested implmentation of presence manager

--- server/realtime/presence.go
+++ server/realtime/presence.go
@@ -1,7 +1,9 @@
 package realtime
 
 import (
+	"encoding/json"
 	"sync"
+	"time"
 
 	"github.com/google/uuid"
 )
@@ -10,13 +12,15 @@ type PresenceManager struct {
 	mu              sync.RWMutex
 	invertedMapping map[uuid.UUID]map[*Client]bool // Subscribed by user
 	forwardMapping  map[uuid.UUID]map[*Client]bool // User subscribed to
+	sessionStore    SessionStore
 	hubSend         chan Envelope
 }
 
-func NewPresenceManager(send chan Envelope) *PresenceManager {
+func NewPresenceManager(store SessionStore, send chan Envelope) *PresenceManager {
 	return &PresenceManager{
 		invertedMapping: make(map[uuid.UUID]map[*Client]bool, 100),
 		forwardMapping:  make(map[uuid.UUID]map[*Client]bool, 100),
+		sessionStore:    store,
 		hubSend:         send,
 	}
 }
@@ -26,7 +30,20 @@ func NewPresenceManager(send chan Envelope) *PresenceManager {
 // 2. Populate inverted mapping for sending status updates
 // 3. Polulate forward mapping for receiving status updates
 func (pm *PresenceManager) SetupUser(id uuid.UUID) {
-	// Task one
+	// Task one - fetch ids from db
+	var ids []uuid.UUID
+
+	// Task 2 - check if they online
+	var clients []*Client
+
+	// Task 3 - populate inverted mapping
+	subscribers := pm.invertedMapping[id]
+	for index := range clients {
+		subscribers[clients[index]] = true
+	}
+
+	// Task 4 - maybe ?
+
 }
 
 // 1. Switch based on status type
@@ -37,16 +54,58 @@ func (pm *PresenceManager) SetupUser(id uuid.UUID) {
 // 1.2.1.2. Find userid in forward mapping
 // 1.2.1.3. Find subscribed to list
 // 1.2.1.4. Use the list a key in inverted mapping and remove userid from it
-func (pm *PresenceManager) SetStatus(status Status) {
+func (pm *PresenceManager) SetStatus(id uuid.UUID, status Status) {
+	var currentStatus StatusEvent
+	currentStatus.UserID = id
+	currentStatus.UpdatedAt = time.Now()
 
+	switch status {
+	case StatusOnline:
+		currentStatus.Status = StatusOnline
+	case StatusOffline:
+		currentStatus.Status = StatusOffline
+	case StatusAway:
+		currentStatus.Status = StatusOffline
+	}
+
+	raw, _ := json.Marshal(currentStatus)
+
+	usermapping := pm.invertedMapping[id]
+	for key := range usermapping {
+		envelope := Envelope{
+			Header: Header{
+				SenderID:   id,
+				ReceiverID: key.Uid,
+				CreatedAt:  time.Now(),
+				TTL:        time.Second * 10,
+			},
+			Data: raw,
+		}
+
+		pm.hubSend <- envelope
+	}
 }
 
 // 1. Add subscriber id to inverted mapping using subscribedto as key
 func (pm *PresenceManager) Subscribe(subscriber, subscribedTo uuid.UUID) {
+	mapping := pm.forwardMapping[subscriber]
+	userClient := pm.sessionStore.Get(subscribedTo)
+	if userClient == nil {
+		// Handle
+		return
+	}
 
+	mapping[userClient] = true
 }
 
 // 1. Remove subscriber id from inverted mapping using subscribedTo id as key
-func (pm *PresenceManager) Unsubscribe(subscrier, subscribedTo uuid.UUID) {
+func (pm *PresenceManager) Unsubscribe(subscriber, subscribedTo uuid.UUID) {
+	mapping := pm.forwardMapping[subscriber]
+	userClient := pm.sessionStore.Get(subscribedTo)
+	if userClient == nil {
+		// Handle
+		return
+	}
 
+	delete(mapping, userClient)
 }
