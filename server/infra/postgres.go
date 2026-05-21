4dee8a0e925feea3f909f7d7c6c239a64c6a03af
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-05-21 14:15:32 +0530

    Added redis and postgress connect function in infra layer

--- server/infra/postgres.go
+++ server/infra/postgres.go
@@ -1,1 +1,17 @@
 package infra
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func NewPostgres(dsn string) *sql.DB {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect to postgress. Error:", err.Error())
+	}
+
+	return db
+}
--- server/infra/redis.go
+++ server/infra/redis.go
@@ -1,1 +1,16 @@
 package infra
+
+import (
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedis(config redis.Options) *redis.Client {
+	client := redis.NewClient(&config)
+	if client == nil {
+		log.Fatal("Failed to connect to redis")
+	}
+
+	return client
+}
