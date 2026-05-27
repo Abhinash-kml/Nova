caf7c58dfb3abcd2320eb9f7987a025254346fc7
Abhinash-kml <74886589+Abhinash-kml@users.noreply.github.com>
2026-05-27 16:54:22 +0530

    Implemeted basic termination signal handling - will improve it later

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Listen for interrupt & kill signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Global context for passing to all services
+	globalCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Perform our task
+
+	// Block untill our signal is trigerred
+	<-signalChan
+
+	// Gracefully shutdown all services by calling cancel() of global context
+	cancel()
 }
